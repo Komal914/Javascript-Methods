edddd8ce9d558f45c4ada6a998665271697c2b27
sanjidah <sanjidah42@gmail.com>
2021-09-30 14:22:46 -0400

    Complete myPush function, works the same as push func

--- methods.js
+++ methods.js
@@ -17,16 +17,36 @@ Array.prototype.myEvery = function () {};
 // REDUCE //
 Array.prototype.myReduce = function () {};
 
-//*******************End of Komal Kaur's functions***************
-
-// INCLUDES //
-Array.prototype.myIncludes = function () {};
+//*******************Start of Sanjidah Abdullah's functions***************
+// INCLUDES: determines whether an array includes a certain value among its entries, returning true or false as appropriate.//
+Array.prototype.myIncludes = function () {
+};
 
 // INDEXOF //
 Array.prototype.myIndexOf = function () {};
 
-// PUSH //
-Array.prototype.myPush = function () {};
+// PUSH: adds one or more elements to the end of an array and returns the new length of the array.//
+Array.prototype.myPush = function(...args){
+	let arg_i = 0;
+    let length = this.length;
+    
+    // last element of 'this' is at length - 1
+	for(let i = length; i < length + args.length; i++){
+        this[i] = args[arg_i]; // add the values of passed in array to 'this' array
+            arg_i++;           // increment index of passed in array (args)
+        }
+	    return this.length;
+    };
+
+// Test
+// const arr = [1,2,3,4,5];
+// console.log("myPush:");
+// console.log(arr.myPush(6,7,8)); // prints 8
+// console.log(arr);               // prints [1,2,3,4,5,6,7,8]
+// console.log("Push:");
+// console.log(arr.push(6,7,8));   // prints 8
+// console.log(arr);               // prints [1,2,3,4,5,6,7,8]
+
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function () {};
