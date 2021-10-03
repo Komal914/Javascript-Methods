d91d1865444d6e1720f83aa953b6073a28818f0f
Komal Kaur <kaur91499@gmail.com>
2021-10-03 01:56:15 -0400

    commented test out

--- methods.js
+++ methods.js
@@ -257,21 +257,19 @@ Object.grabValues = function () {
 // // expected output: "c"
 
 //********************My Reducer Test***********
-const array1 = [1, 2, 3, 4];
-const reducer = (previousValue, currentValue) => previousValue + currentValue;
-
-1 + 2 + 3 + 4;
-console.log("Correct:", array1.reduce(reducer));
-// expected output: 10
-console.log("Mine:", array1.myReduce(reducer));
-
-// 5 + 1 + 2 + 3 + 4
-console.log("Correct 2:", array1.reduce(reducer, 5));
-console.log("MINE", array1.myReduce(reducer, 5));
+// const array1 = [1, 2, 3, 4];
+// const reducer = (previousValue, currentValue) => previousValue + currentValue;
+//
+// 1 + 2 + 3 + 4;
+// console.log("Correct:", array1.reduce(reducer));
+// // expected output: 10
+// console.log("Mine:", array1.myReduce(reducer));
+//
+// // 5 + 1 + 2 + 3 + 4
+// console.log("Correct 2:", array1.reduce(reducer, 5));
+// console.log("MINE", array1.myReduce(reducer, 5));
 
 // expected output: 15
-
-//another test
 
 //********************My Includes Test***********
 // const arr = [1,2,3,4,5];
